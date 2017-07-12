6fa8bdfffea3ff1b011beea7ace34b50df1f0b85
Nick Fallon <nickfallonwastaken@gmail.com>
2017-07-12 12:39:38 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <button><img src="./assets/logo.png" id="logo"></button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 10rem;
+}
+#logo {
+  height: 50vh;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Plague Nation',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: bold;
+  color: black;
+}
+</style>
