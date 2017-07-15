4aac1c00cbac8ea737ef634218d21371bfe4ad86
Nick Fallon <nickfallonwastaken@gmail.com>
2017-07-14 23:44:29 -0600

    Added splash page and styling

---
+++ src/components/Splash.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="splash-main" id="splashPage">
+    <div class="main-title">
+      <router-link to="/map"><button class="title-button">Plague Nation</button></router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'splashPage'
+}
+</script>
+
+<style>
+  .title-button {
+    width: 50vw;
+    height: 10vw;
+    font-size: 1em;
+    border-radius: 10px;
+    box-shadow: 0px 0px 2em #FFF;
+    font-family: "Special Elite", Helvetica, sans-serif;
+    padding: 10px;
+  }
+  .title-button:hover {
+    cursor: pointer;
+    background-color: #191A1A;
+    color: #D11A02;
+    box-shadow: 0px 0px 3em #D11A02;
+  }
+  .title-button:focus {
+    outline: 0;
+  }
+  .main-title {
+    text-align: center;
+    font-size: 5rem;
+    font-family: "Special Elite", Helvetica, sans-serif;
+    margin-top: 35vh;
+    /*padding-top: 35vh;
+    padding-bottom: 60vh;
+    background-color: #191A1A;
+    color: white;
+    padding-left: 100px;
+    padding-right: 100px;*/
+  }
+  body {
+    background-color: #191A1A;
+    height: 100vh;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Mapbox from '@/components/Mapbox';
+import Splash from '@/components/Splash';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'Splash',
-    //   component: Splash,
-    // },
+    {
+      path: '/',
+      name: 'Splash',
+      component: Splash,
+    },
     {
       path: '/map',
       name: 'Mapbox',
